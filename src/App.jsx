73f8b011d65f73bdf3c14b2cecbaea62b748e30a
hafizswabah <swabah.a.avd121@gmail.com>
2024-01-04 20:47:19 +0530

    nav icons added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <NavBar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './navbar.css'
+import { IoHomeOutline } from "react-icons/io5";
+import { HiOutlineLogout } from "react-icons/hi";
+import { IoIosCalculator } from "react-icons/io";
+import { IoCloudUploadOutline } from "react-icons/io5"
+function NavBar() {
+    return (
+        <>
+            <div className="nav-bar">
+                <div className="nav-items">
+                    <IoHomeOutline className='Home-Icon' />
+                    <span>Home</span>
+                </div>
+                <div className="nav-items">
+                    <IoCloudUploadOutline className='Home-Icon' />
+                    <span>whether</span>
+                </div>
+                <div className="nav-items">
+                    <IoIosCalculator className='Home-Icon' />
+                    <span>Calculator</span>
+                </div>
+                <div className="nav-items">
+                    <HiOutlineLogout className='Home-Icon' />
+                    <span>Log Out</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
