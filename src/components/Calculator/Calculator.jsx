0e0a8c2e5c793294a27fbae52c0e625b58ab04cd
hafizswabah <swabah.a.avd121@gmail.com>
2024-01-15 22:22:40 +0530

    ui updated

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -9,16 +9,27 @@ function Calculator() {
 
             </div>
             <div className="keyboard">
+                <div className="layer">
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
+                </div>
+                <div className="layer">
                 <button>4</button>
                 <button>5</button>
                 <button>6</button>
+                </div>
+                <div className="layer">
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
+                </div>
+                <div className="layer">
+                <button>+</button>
+                <button>-</button>
                 <button>0</button>
+                </div>
+        
             </div>
         </div>
         </div>
