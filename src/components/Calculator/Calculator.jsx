532659d0a99816e8e7a43f24551d1370be490924
hafizswabah <swabah.a.avd121@gmail.com>
2024-01-17 13:19:19 +0530

    calculator ui changed

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -10,24 +10,32 @@ function Calculator() {
             </div>
             <div className="keyboard">
                 <div className="layer">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
+                <button>AC</button>
+                <button>%</button>
+                <button>$</button>
+                <button className='orange-key'>%</button>
                 </div>
-                <div className="layer">
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                </div>
-                <div className="layer">
+                <div className="numKeys">
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
+                <button className='orange-key'>*</button>
+                </div>
+                <div className="numKeys">
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button className='orange-key'>-</button>
+                </div>
+                <div className="numKeys">
+          
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button className='orange-key'>+</button>
                 </div>
                 <div className="function_Keys">
-                <button>+</button>
-                <button>-</button>
-                <button>=</button>
+                <button className='sum'>=</button>
                 <button>.</button>
                 <button>0</button>
                 </div>
